6210371fc2695906b18e113bbedbf1f1fcdce651
Mkazuhikovakassugui <marcio.kazuhiko.vakassugui@gmail.com>
2022-04-23 10:06:53 -0300

    Acrescenta sexta cor à função cores_olimpiadas

--- R/tema_olimpiadas.R
+++ R/tema_olimpiadas.R
@@ -6,6 +6,40 @@
 # 1) Função para o tema dos gráficos ----------------------------------------------------------
 
 
-tema_olimpíadas <- function(){
-  
+tema_olimpiadas <- function(){
+  theme(
+    text = element_text(
+      family = "Rio 2016"
+    ),                                              
+    title = element_text(
+      size = 15,
+      color = 
+    ),                                               
+    legend.position = "none",
+    axis.text.x=element_text(
+      size=11
+    ),
+    axis.text.y=element_text(
+      size=11
+    ),
+    plot.title = element_text(
+      hjust = 0.5
+    )
+  )
+}
+
+
+# 2) Função para definição das cores olimpíadas -----------------------------------------------
+
+cores_olimpiadas <- function(variables) {
+  scale_fill_manual(
+    values = c(
+      "#0085C8",
+      "#F3C200",
+      "#009E3C",
+      "#E00024",
+      "#000000",
+      "#2218DE"
+    )
+  )
 }
